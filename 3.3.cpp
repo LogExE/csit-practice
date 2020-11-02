88f5844f0bece1a1d9d541abcdb6fd7a210b627c
Vladimir <vladimir220022@mail.ru>
2020-11-02 21:25:07 +0400

    Create 3.3.cpp

---
+++ 3.3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+	double p, s;
+	std::cin >> p >> s;
+	if (p <= 0 || s <= 0) 
+	{
+		std::cout << "invalid data";
+		return 0;
+	}
+	double d = 2 * sqrt(s / acos(-1));
+	double a = p / 4;
+	double e = 1e-12;
+	if (abs(d - a) < e)
+	{
+		std::cout << "d = a";
+	}
+	else if (d > a + e) 
+	{
+		std::cout << "d > a";
+	}
+	else
+	{
+		std::cout << "d < a";
+	}
+}
