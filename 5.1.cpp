85b7b8103cdf164f6d7f61c2664e55449f1e4ce2
Vladimir <vladimir220022@mail.ru>
2020-11-30 20:24:58 +0400

    Create 5.1.cpp

---
+++ 5.1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+	int n;
+	std::cin >> n;
+	int* a = new int[n];
+	for (int i = 0; i < n; ++i)
+	{
+		std::cin >> a[i];
+	}
+	if (n % 2 == 0) 
+	{
+		int t = a[n / 2 - 1];
+		a[n / 2 - 1] = a[n / 2];
+		a[n / 2] = t;
+	}
+	else 
+	{
+		a[n / 2] *= 2;
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		std::cout << a[i] << ' ';
+	}
+}
