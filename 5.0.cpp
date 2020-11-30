944eeee1a3d2ddee267a1789f8c70008a4433e31
Vladimir <vladimir220022@mail.ru>
2020-11-30 20:18:30 +0400

    Create 5.0.cpp

---
+++ 5.0.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+	int n, k;
+	std::cin >> n >> k;
+	int *a = new int[n];
+	for (int i = 0; i < n; ++i) 
+	{
+		std::cin >> a[i];
+	}
+	int *b = new int[k];
+	for (int i = 0; i < k; ++i) 
+	{
+		std::cin >> b[i];
+	}
+	for (int i = 0; i < k; ++i)
+	{
+		a[b[i] - 1] = 0;
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		std::cout << a[i] << ' ';
+	}
+}
