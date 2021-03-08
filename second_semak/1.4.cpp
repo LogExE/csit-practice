66094a815725ca1e9792566164ad8c1598f77129
LogXx <awesome149712@gmail.com>
2021-03-08 22:17:04 +0400

    Update 1.4.cpp

--- second_semak/1.4.cpp
+++ second_semak/1.4.cpp
@@ -42,6 +42,7 @@ double dist(point3d p1, point3d p2)
 	return dist(p1.x, p1.y, p1.z, p2.x, p2.y, p2.z);
 }
 
+/* зачем, если есть версии для double? :)
 double dist(int x, int y)
 {
 	return dist(x, y, 0);
@@ -61,6 +62,7 @@ double dist(int x1, int y1, int z1, int x2, int y2, int z2)
 {
 	return sqrt(square(x1 - x2) + square(y1 - y2) + square(z1 - z2));
 }
+*/
 
 double dist(double x, double y)
 {
@@ -74,7 +76,7 @@ double dist(double x1, double y1, double x2, double y2)
 
 double dist(double x, double y, double z)
 {
-	return dist(x, y, z, 0, 0, 0);
+	return dist(x, y, z, 0.0, 0.0, 0.0);
 }
 
 double dist(double x1, double y1, double z1, double x2, double y2, double z2)
